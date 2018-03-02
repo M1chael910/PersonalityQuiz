8e8a023e0112d146d97bda479ede2e927cd7d97f
Michael Murphy <mmurphy0263@jh-mba158-18.d214.org>
2018-03-02 11:42:06 -0600

    COmmit

--- PersonalityQuiz/ViewController.swift
+++ PersonalityQuiz/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var segueIdentifier = "mainScreenToNext"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func beginButtonPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: segueIdentifier, sender: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
