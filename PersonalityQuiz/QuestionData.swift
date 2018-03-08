a39d372be6666bc25ac9366d58b13d71ed218b2d
Michael Murphy <mmurphy0263@jh-mba158-18.local>
2018-03-08 11:42:25 -0600

    Commit

---
+++ PersonalityQuiz/QuestionData.swift
@@ -0,0 +1,50 @@
+//
+//  QuestionData.swift
+//  PersonalityQuiz
+//
+//  Created by Michael Murphy on 3/7/18.
+//  Copyright © 2018 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+
+
+struct Question {
+    var text: String
+    var type: ResponseType
+    var answers: [Answer]
+}
+
+
+enum ResponseType {
+    case single, multiple, ranged
+}
+
+
+
+struct Answer {
+    var text: String
+    var type: AnimalType
+}
+
+
+
+enum AnimalType: Character {
+    case dog = "🐶", cat = "🐱", rabbit = "🐰", turtle = "🐢"
+
+    var definition: String {
+        switch self {
+        case .dog:
+            return "You are incredibly outgoing. You surround yourself with the people you love and enjoy activities with your friends."
+        case .cat:
+            return "Mischievous, yet mild-tempered, you enjoy doing things on your own terms."
+        case .rabbit:
+            return "You love everything that's soft. You are healthy and full of energy."
+        case .turtle:
+            return "You are wise beyond your years, and you focus on the details. Slow and steady wins the race."
+        }
+    }
+}
+
+
