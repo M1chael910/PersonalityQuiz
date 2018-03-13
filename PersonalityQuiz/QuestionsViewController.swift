93eaa4efa20eb1fe0101eb24461d0edf527cc4b2
Michael Murphy <mmurphy0263@jh-mba158-18.d214.org>
2018-03-13 11:30:37 -0500

    Commit

--- PersonalityQuiz/QuestionsViewController.swift
+++ PersonalityQuiz/QuestionsViewController.swift
@@ -11,21 +11,13 @@ import UIKit
 class QuestionsViewController: UIViewController {
     
     
+    
     @IBOutlet weak var multipleStackView: UIStackView!
-    
     @IBOutlet weak var multipleLabel1: UILabel!
-    
     @IBOutlet weak var multipleLabel2: UILabel!
-    
     @IBOutlet weak var multipleLabel3: UILabel!
-    
     @IBOutlet weak var multipleLabel4: UILabel!
-    
-    
     @IBOutlet weak var QuestionLabel: UILabel!
-    
-    
-    
     
     @IBOutlet weak var singleStackView: UIStackView!
     @IBOutlet weak var SingleButton1: UIButton!
@@ -35,16 +27,21 @@ class QuestionsViewController: UIViewController {
     @IBOutlet weak var progressBar: UIProgressView!
     
     
-    
     @IBOutlet weak var rangedStackView: UIStackView!
-    
     @IBOutlet weak var rangedLabel1: UILabel!
-    
-    
     @IBOutlet weak var rangedLabel2: UILabel!
     
     
+    @IBOutlet weak var multiSwitch1: UISwitch!
+    @IBOutlet weak var multiSwitch2: UISwitch!
+    @IBOutlet weak var multiSwitch3: UISwitch!
+    @IBOutlet weak var multiSwitch4: UISwitch!
     
+    
+    @IBOutlet weak var rangedSlider: UISlider!
+    
+    
+    var answersChosen: [Answer] = []
     
     var questionIndex = 0
     
@@ -79,16 +76,77 @@ class QuestionsViewController: UIViewController {
             ])
     ]
     
-    func getTextViewData() {
-
-    }
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func multipleAnswerButtonPressed() {
+    let currentAnswers = questions[questionIndex].answers
+    
+        if multiSwitch1.isOn {
+            answersChosen.append(currentAnswers[0])
+        }
+        if multiSwitch2.isOn {
+            answersChosen.append(currentAnswers[1])
+        }
+        if multiSwitch3.isOn {
+            answersChosen.append(currentAnswers[2])
+        }
+        if multiSwitch4.isOn {
+            answersChosen.append(currentAnswers[3])
+        }
+        
+        nextQuestion()
+    }
+    
+    @IBAction func rangedAnswerButtonPressed() {
+        let currentAnswers = questions[questionIndex].answers
+        let index = Int(round(rangedSlider.value * Float(currentAnswers.count - 1)))
+        answersChosen.append(currentAnswers[index])
+        
+        nextQuestion()
+    }
+    
+    
+    func nextQuestion() {
+        questionIndex += 1
+        
+        if questionIndex < questions.count {
+            updateUI()
+        } else {
+            performSegue(withIdentifier: "ResultsSegue", sender: nil)
+        }
+    }
+    
+    
+    
+    @IBAction func SingleAnswerButtonPressed(_ sender: UIButton) {
+        let currentAnswers = questions[questionIndex].answers
+        
+        switch sender {
+        case SingleButton1:
+            answersChosen.append(currentAnswers[0])
+        case singleButton2:
+            answersChosen.append(currentAnswers[1])
+        case singleButton3:
+            answersChosen.append(currentAnswers[2])
+        case singleButton4:
+            answersChosen.append(currentAnswers[3])
+        default:
+            break
+        }
+        
+        nextQuestion()
+        
+    }
+    
+    
     
     
     func updateUI() {
@@ -106,10 +164,44 @@ class QuestionsViewController: UIViewController {
         progressBar.setProgress(totalProgress, animated: true)
         
         
-        
-        
+        switch currentQuestion.type {
+        case .single:
+            updateSingleStack(using: currentAnswers)
+        case .multiple:
+            updateMultipleStack(using: currentAnswers)
+        case .ranged:
+            updateRangedStack(using: currentAnswers)
+        }
         
     }
+    func updateRangedStack(using answers: [Answer]) {
+        rangedStackView.isHidden = false
+        rangedLabel1.text = answers.first?.text
+        rangedLabel2.text = answers.last?.text
+    }
+    
+    
+    
+    func updateSingleStack(using answers: [Answer]) {
+        singleStackView.isHidden = false
+        SingleButton1.setTitle(answers[0].text, for: .normal)
+        singleButton2.setTitle(answers[1].text, for: .normal)
+        singleButton3.setTitle(answers[2].text, for: .normal)
+        singleButton4.setTitle(answers[3].text, for: .normal)
+    }
+    
+    
+    
+    func updateMultipleStack(using answers: [Answer]) {
+        multipleStackView.isHidden = false
+        multipleLabel1.text = answers[0].text
+        multipleLabel2.text = answers[1].text
+        multipleLabel3.text = answers[2].text
+        multipleLabel4.text = answers[3].text
+    }
+    
+   
+    
     
     
     override func didReceiveMemoryWarning() {
